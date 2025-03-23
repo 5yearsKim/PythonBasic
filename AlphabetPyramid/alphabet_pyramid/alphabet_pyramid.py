9cdb955debd15e2a5a47f132091ddb898ef37059
onion.kim <onion.kim@intel.com>
2025-03-24 00:23:49 +0900

    hide the answer
    Signed-off-by: onion.kim <onion.kim@intel.com>

--- AlphabetPyramid/alphabet_pyramid/alphabet_pyramid.py
+++ AlphabetPyramid/alphabet_pyramid/alphabet_pyramid.py
@@ -4,12 +4,8 @@ from .pyramid_drawer import PyramidDrawer
 
 class AlphabetPyramid:
     def __init__(self) -> None:
-        self.counter = AlphabetCounter()
-        self.drawer = PyramidDrawer()
+        pass
 
     def run(self) -> None:
-        while True:
-            s = input("Enter a string: ")
-            max_char, max_count = self.counter.count(s)
-            answer = self.drawer.draw(max_char, max_count)
-            print(answer)
+        pass
+
